812dc854d285610b9b5a631e8d0a25d416b317a1
bashbunni <bashbunni.io@gmail.com>
2021-05-01 23:02:33 -0700

    cleaned up tests

--- lesson1/hello_test.go
+++ lesson1/hello_test.go
@@ -3,26 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := HelloWorld()
-	want := "hello world"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("ello world", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("hello by name", func(t *testing.T) {
+		got := Hello("bashbunni", "")
+		want := "Hello bashbunni"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("name in french", func(t *testing.T) {
+		got := Hello("bash", "french")
+		want := "Bonjour bash"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func TestUrMom(t *testing.T) {
 	got := UrMom()
-	want := "hello world"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-
-func TestHelloName(t *testing.T) {
-	got := Hello("bashbunni")
-	want := "Hello, bashbunni"
+	want := "ur mom"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
